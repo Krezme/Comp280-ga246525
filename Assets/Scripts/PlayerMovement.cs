cfb9aaa6ec70c3582fe288c216a6898128d0e7c3
Krezme <karakonjo5818@gmail.com>
2021-09-25 19:18:55 +0100

    Added jump functionality

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,14 +6,16 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController characterController;
 
-
+    
 
     public float speed = 10f;
 
     public float gravity = -10f;
 
+    public float jumpHeight = 5f;
+
     public Transform groundCheck;
-    public float groundDistance = 0.4f;
+    public float groundDistance = 1f;
     public LayerMask goundLayer;
     bool isGrounded;
 
@@ -23,6 +25,7 @@ public class PlayerMovement : MonoBehaviour
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, goundLayer);
 
         if (isGrounded && velocity.y < 0f) {
+            Debug.Log("isGrounded");
             velocity.y = -1f;
         }
 
@@ -32,6 +35,10 @@ public class PlayerMovement : MonoBehaviour
         Vector3 move = transform.right * x + transform.forward * z;
 
         characterController.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded) {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
 
         velocity.y += gravity * Time.deltaTime;
 
