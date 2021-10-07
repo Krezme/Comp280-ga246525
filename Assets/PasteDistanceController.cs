1ed4c844ffe2bf564d294655bb282059d0a02377
GEORGI Aleksandrov <ga246525@falmouth.ac.uk>
2021-10-07 15:48:27 +0100

    Slider Functionality added
    The slider also updates when the scroll is used

---
+++ Assets/PasteDistanceController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PasteDistanceController : MonoBehaviour
+{
+    
+    float distanceValue = 0f;
+
+    private Slider distanceSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        distanceSlider = FindObjectOfType<Canvas>().GetComponent<UIReferences>().slider.GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+            
+        distanceValue += Input.mouseScrollDelta.y / 25;
+        distanceValue = Mathf.Clamp(distanceValue, 0, 1);
+
+        distanceSlider.value = distanceValue;
+
+        Debug.Log(distanceValue);
+    }
+}
