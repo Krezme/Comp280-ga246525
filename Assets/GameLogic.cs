6245a77962c4702ab16d40bc4d7d02360ec5ba9a
GA246525 <ga246525@falmouth.ac.uk>
2021-12-16 18:21:36 +0000

    Removed Forge

--- Assets/GameLogic.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using BeardedManStudios.Forge.Networking.Unity;
-public class GameLogic : MonoBehaviour
-{
-    private void Start()
-    {
-        NetworkManager.Instance.InstantiateCubeSpinner();
-    }
-}
